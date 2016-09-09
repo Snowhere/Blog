209b86873adc0d64eb8c3b96f20ac2d6fc6cbab3
snowhere <459904593@qq.com>
2016-09-09 18:11:56 +0800

    111111111111111

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
-var matrix = ['1', '0', '1', '1', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1'];
-var target = ['1', '0', '1', '1', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', '1', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1', '0', '1', '1', '0', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '1'];
-var sort = [];
 var matrix=[
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0]
 ];
 var target = [
     [1, 0],
@@ -21,6 +18,9 @@ var target = [
     [0, 1]
 ];
 
+/**
+ *  一维化正则式匹配算法
+ */
 //一维化
 var matrixStr = '';
 for(var i=0;i<matrix.length;i++){
@@ -39,6 +39,7 @@ targetStr+=target[j].join('');
 console.log(targetStr);
 
 //get reg
+var sort = [];
 var count = 1;
 for (var i = 0; i < targetStr.length - 1; i++) {
     if (targetStr[i] == targetStr[i + 1]) {
@@ -65,20 +66,56 @@ console.log(pattern);
 
 var reg = new RegExp(pattern);
 
-
-function canPut(matrixStr, reg, tableWidth, brickWidth) {
+var timeOne;
+console.time(timeOne);
+function canPut(matrixStr, reg, tableWidth, brickWidth,lastIndex) {
     var index = matrixStr.search(reg);
     if (index == -1) {
         return false;
     } else {
         if (parseInt(index / tableWidth) == parseInt((index + brickWidth-1) / tableWidth)) {
-            return true;
+            return index;
         } else {
-            return canPut(matrixStr.slice(index + 1), reg, tableWidth, brickWidth);
+            return canPut(matrixStr.slice(index + 1), reg, tableWidth, brickWidth,lastIndex+index);
         }
     }
 }
+var result = canPut(matrixStr,reg,matrix[0].length,target[0].length,0);
+console.log([parseInt(result/11)+1,result%11]);
+console.timeEnd(timeOne);
 
 
-var result = canPut(matrixStr,reg,matrix[0].length,target[0].length);
-console.log(result);
+
+var timeTwo;
+console.time(timeTwo);
+/**
+ *  遍历算法时间
+ */
+function traverse(matrix, target) {
+    for (var i = 0; i <= matrix.length - target.length; i++) {
+        for (var j = 0; j <= matrix[0].length - target[0].length; j++) {
+            //定点i,j
+            var rt=check(i,j);
+            if(rt){
+                return rt;
+            }
+        }
+    }
+    return false;
+}
+function check(i, j) {
+    for (var n = 0; n < target.length; n++) {
+        for (var m = 0; m < target[0].length; m++) {
+            if (matrix[i + n][j + m] && target[n][m]) {
+                return false;
+            }
+        }
+    }
+    return [i,j];
+}
+
+
+
+var result2  = traverse(matrix,target);
+console.log(result2);
+console.timeEnd(timeTwo);
