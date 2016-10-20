e67f55ee68df0e4eafadd60b8e32aa5fc4ab9931
snowhere <459904593@qq.com>
2016-10-20 18:15:19 +0800

    finish

--- map/src/main/java/run/Main.java
+++ map/src/main/java/run/Main.java
@@ -19,9 +19,9 @@ public class Main {
         arp.addMapping("c_area", AreaModel.class);
         arp.start();
 
-        getArea();
+        //getArea();
         //从数据库中最后一条数据获取当前爬取的url信息,然后继续爬取.
-        //getPois();
+        getPois();
     }
 
     private static void getArea() {
@@ -44,13 +44,15 @@ public class Main {
         Task task = new Task();
         POIModel model = POIModel.model
             .findFirst("SELECT  * FROM `c_poi` ORDER BY id desc limit 1");
+        AreaModel lastArea = AreaModel.model
+            .findFirst("select * from c_area order by id desc");
         int areaId = 1;
         int page = 1;
         if (model != null) {
             areaId = model.getInt("area_id");
             page = model.getInt("page") + 1;
         }
-        while (areaId <= 2803) {
+        while (areaId <= lastArea.getInt("id")) {
             //获取区域块
             AreaModel area = AreaModel.model.findById(areaId);
             int num = area.getInt("num");
