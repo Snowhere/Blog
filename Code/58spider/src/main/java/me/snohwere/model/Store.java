1c320a2518f02c23869445c2b8d993eec0af861d
STH <459904593@qq.com>
2017-05-06 15:07:58 +0800

    update

--- Code/58spider/src/main/java/me/snohwere/model/Store.java
+++
@@ -1,9 +0,0 @@
-package me.snohwere.model;
-
-import com.jfinal.plugin.activerecord.Model;
-
-@SuppressWarnings("serial")
-public class Store extends Model<Store> {
-
-    public static Store dao = new Store();
-}
