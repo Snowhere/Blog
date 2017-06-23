b5891edefbc56e2b031ebc665f5d0c7212fbd3e1
SunTenghao <tenghao.sun@cutt.com>
2017-06-23 18:37:00 +0800

    blog

--- Code/Code/src/test/Code.java
+++ Code/Code/src/test/Code.java
@@ -1,27 +1,20 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Code {
 
 	public static void main(String[] args) {
-	    List<String> list = new ArrayList<>();
-	    list.add("1");
-	    list.add("2");
-	    list.add("3");
-	    list.add("4");
-	    list.add("5");
-	    for (int i=0; i < list.size();i++) {
-	        String dim = list.get(i);
-            if (dim.equals("3") || dim.equals("3")) {
-                list.remove(i);
-                break;
-            }
-        }
-	    for(int i=0; i < list.size();i++){
-	        System.out.println(list.get(i));;
-	    }
+		String url = "【妙趣水桶包 不规则包型更有大容量】，点击链接再选择浏览器打开http://c.b1wv.com/h.iI8vkW?cv=kG40Zw5QAdg&sm=5cf31f ，或复制这条信息￥kG40Zw5QAdg￥后打开手机淘宝";
+		//Pattern pattern = Pattern.compile("(http|ftp|https|www)[^\u4e00-\u9fa5\\s]*?\\.(com|net|cn|me|tw|fr)[^\u4e00-\u9fa5\\s]*");
+		Pattern pattern = Pattern.compile("(http|ftp|https|www)[^\u4e00-\u9fa5\\s]*?\\.(com|net|cn|me|tw|fr)([a-zA-Z0-9\\-\\._\\?\\,\\'/\\\\\\+&amp;%\\$#\\=~])*");
+		Matcher matcher = pattern.matcher(url);
+		if (matcher.find()) {
+			url = matcher.group(0);
+		}
+		System.out.println(url);
+
 	}
 	
 	
