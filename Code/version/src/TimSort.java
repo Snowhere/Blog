52c0c30c34f9982df52b22b6154943401c1979ba
STH <459904593@qq.com>
2019-07-02 01:12:27 +0800

    version

---
+++ Code/version/src/TimSort.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class TimSort {
+    public static void main(String[] args) {
+        int[] sample = new int[]
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2, 1, 0, -2, 0, 0, 0, 0};
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : sample)
+            list.add(i);
+        // use the native TimSort in JDK 7
+        Collections.sort(list, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                // miss the o1 = o2 case on purpose
+                return o1 > o2 ? 1 : -1;
+            }
+        });
+    }
+}
